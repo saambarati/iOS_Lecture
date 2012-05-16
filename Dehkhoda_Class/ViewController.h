be76ffac8940e136c1a660ef14c113a2ef298cfa
Saam Barati <saambarati1@gmail.com>
2012-05-16 00:02:26 -0700

    Initial Commit

---
+++ Dehkhoda_Class/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dehkhoda_Class
+//
+//  Created by Saam Barati on 5/16/12.
+//  Copyright (c) 2012 Saam Barati Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Dehkhoda_Class/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  Dehkhoda_Class
+//
+//  Created by Saam Barati on 5/16/12.
+//  Copyright (c) 2012 Saam Barati Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+  return YES;
+}
+
+@end
